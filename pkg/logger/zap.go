478d22f3f25c3b0e4975c44d91f352d2200d4d0b
Carlos Marcelo Rodriguez <marcelo@voidspace.us>
2021-12-28 18:19:22 -0600

    remove logging comments

--- pkg/logger/zap.go
+++ pkg/logger/zap.go
@@ -44,44 +44,6 @@ func initLog() (zap.Logger, error) {
 	encoder := zapcore.NewConsoleEncoder(config)
 	atom = zap.NewAtomicLevel()
 	logr := zap.New(zapcore.NewCore(encoder, zapcore.Lock(os.Stdout), atom))
-	/*
-	   	rawJSON := []byte(`{
-	   	 "level": "info",
-	        "Development": true,
-	        "DisableCaller": false,
-	   	 "encoding": "console",
-	   	 "outputPaths": ["stdout", "./log.txt"],
-	   	 "errorOutputPaths": ["stderr"],
-	   	 "encoderConfig": {
-	   		"timeKey":        "ts",
-	   		"levelKey":       "level",
-	   		"messageKey":     "msg",
-	           "nameKey":        "name",
-	   		"stacktraceKey":  "stacktrace",
-	           "callerKey":      "caller",
-	   		"lineEnding":     "\n\t",
-	           "timeEncoder":     "iso8601",
-	   		"levelEncoder":    "lowercaseLevel",
-	           "durationEncoder": "stringDuration",
-	           "callerEncoder":   "shortCaller"
-	   	 }
-	   	}`)
 
-	   	var cfg zap.Config
-	   	var zLogger *zap.Logger
-	   	var err error
-	   	//standard configuration
-	   	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-	   		return *zLogger, errors.Wrap(err, "Unmarshal")
-	   	}
-
-	   	zLogger, err = cfg.Build()
-	   	if err != nil {
-	   		return *zLogger, errors.Wrap(err, "cfg.Build()")
-	   	}
-
-	   	zLogger.Debug("logger construction succeeded")
-	   	return *zLogger, nil
-	*/
 	return *logr, nil
 }
