81c2b131b6f9094860bed890e431b7b60925f1b8
Carlos Marcelo Rodriguez <marcelo@voidspace.us>
2021-12-30 13:14:52 -0600

    added consumer and producer stats

--- pkg/consumer/stats.go
+++ pkg/consumer/stats.go
@@ -2,16 +2,24 @@ package consumer
 
 import (
 	"github.com/Shopify/sarama"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/atomic"
 )
 
 type StatsInterceptor struct {
-	total atomic.Uint64
+	total            atomic.Uint64
+	promTotalCounter prometheus.Counter
 }
 
 func newStats() *StatsInterceptor {
 	// Initialize metrics counter
-	s := &StatsInterceptor{}
+	s := &StatsInterceptor{
+		promTotalCounter: promauto.NewCounter(prometheus.CounterOpts{
+			Name: "gibson_consumer_events_total",
+			Help: "The total number consumed events",
+		}),
+	}
 	s.total.Store(0)
 	return s
 }
@@ -22,4 +30,5 @@ func (s *StatsInterceptor) GetTotal() uint64 {
 
 func (s *StatsInterceptor) OnConsume(msg *sarama.ConsumerMessage) {
 	s.total.Inc()
+	s.promTotalCounter.Inc()
 }
--- pkg/producer/stats.go
+++ pkg/producer/stats.go
@@ -2,16 +2,24 @@ package producer
 
 import (
 	"github.com/Shopify/sarama"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/atomic"
 )
 
 type StatsInterceptor struct {
-	total atomic.Uint64
+	total            atomic.Uint64
+	promTotalCounter prometheus.Counter
 }
 
 func newStats() *StatsInterceptor {
 	// Initialize metrics counter
-	s := &StatsInterceptor{}
+	s := &StatsInterceptor{
+		promTotalCounter: promauto.NewCounter(prometheus.CounterOpts{
+			Name: "gibson_producer_events_total",
+			Help: "The total number produced events",
+		}),
+	}
 	s.total.Store(0)
 	return s
 }
@@ -22,4 +30,5 @@ func (s *StatsInterceptor) GetTotal() uint64 {
 
 func (s *StatsInterceptor) OnSend(msg *sarama.ProducerMessage) {
 	s.total.Inc()
+	s.promTotalCounter.Inc()
 }
